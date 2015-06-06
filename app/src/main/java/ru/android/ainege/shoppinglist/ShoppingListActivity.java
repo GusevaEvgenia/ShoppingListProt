0b02f0e9b2a533694999ded02b9fc0dafa0d75cd
Evgenia <Ainege@gmail.com>
2015-06-06 20:35:22 +0300

    single fragment Activity

--- app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListActivity.java
@@ -1,26 +1,17 @@
 package ru.android.ainege.shoppinglist;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by Belkin on 06.06.2015.
  */
-public class ShoppingListActivity extends FragmentActivity {
+public class ShoppingListActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_container);
-
-        FragmentManager fm = getFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-
-        if(fragment == null){
-            fragment = new ShoppingListFragment();
-            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new ShoppingListFragment();
     }
 }
