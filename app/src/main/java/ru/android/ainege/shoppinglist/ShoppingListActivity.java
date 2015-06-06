56b5800ac6d0f78ee3d880df61135865b881c53d
Evgenia <Ainege@gmail.com>
2015-06-06 20:16:06 +0300

    add fragment to activity

--- app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListActivity.java
@@ -1,5 +1,7 @@
 package ru.android.ainege.shoppinglist;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -12,5 +14,13 @@ public class ShoppingListActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_container);
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null){
+            fragment = new ShoppingListFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
 }
