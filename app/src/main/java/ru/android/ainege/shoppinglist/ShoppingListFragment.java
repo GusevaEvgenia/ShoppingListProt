4a4077f2ee07324b089a84d7f25f118967e3c8d4
Evgenia <Ainege@gmail.com>
2015-06-07 12:59:51 +0300

    do listView in shoppingList

--- app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListFragment.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListFragment.java
@@ -1,18 +1,27 @@
 package ru.android.ainege.shoppinglist;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by Belkin on 06.06.2015.
  */
-public class ShoppingListFragment extends Fragment{
+public class ShoppingListFragment extends ListFragment {
 
-    @Nullable
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                                                                android.R.layout.simple_list_item_1,
+                                                                getResources().getStringArray(R.array.units));
+        setListAdapter(adapter);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.shopping_list, container, false);
