ebcae64c4916c3ea91887fdbe9ab2ae23941b271
Evgenia <Ainege@gmail.com>
2015-06-07 13:51:32 +0300

    add own adapter to shopping list

--- app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListFragment.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ShoppingListFragment.java
@@ -6,6 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Belkin on 06.06.2015.
@@ -16,15 +22,50 @@ public class ShoppingListFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                                                                android.R.layout.simple_list_item_1,
-                                                                getResources().getStringArray(R.array.units));
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Мясо1");
+        list.add("Мясо2");
+        list.add("Мясо3");
+        list.add("Мясо4");
+
+        ItemAdapter adapter = new ItemAdapter(list);
         setListAdapter(adapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.shopping_list, container, false);
+        View v = inflater.inflate(R.layout.activity_shopping_list, container, false);
         return v;
+    }
+
+    private class ItemAdapter extends ArrayAdapter<String>{
+        public ItemAdapter(ArrayList<String> items) {
+            super(getActivity(), 0, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.shopping_list_item, null);
+            }
+
+            String s = getItem(position);
+
+            CheckBox buyCheckBox = (CheckBox) convertView.findViewById(R.id.itemBuy_checkBox);
+            TextView nameTextView = (TextView) convertView.findViewById(R.id.itemName_textView);
+            nameTextView.setText(s);
+            TextView numberTextView = (TextView) convertView.findViewById(R.id.itemNumber_textView);
+            numberTextView.setText("2 шт.");
+            TextView priceTextView = (TextView) convertView.findViewById(R.id.itemPrice_textView);
+            priceTextView.setText("150");
+
+            return convertView;
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        //TODO edit item+++++
+        Toast.makeText(getActivity(), "Выбрана: " + getListAdapter().getItem(position), Toast.LENGTH_SHORT).show();
     }
 }
